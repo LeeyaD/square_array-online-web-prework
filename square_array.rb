b1624fb7116e1835db541c974d943ccdb9b198bd
Leeya Davis <leeya463@gmail.com>
2019-02-16 23:09:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,11 @@
 numbers = [1, 3, 5]
 
 def square_array(numbers)
+  new_array = []
+  numbers.each { |sq| new_array.("sq*sq")}
+  new_array
+end
+
+def square_array(numbers)
   numbers.collect { |sq| sq*sq}
 end
